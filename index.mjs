eb70f599c7dee471c2a7ce7b252e531804a640ec
Michael Sprague <michael@sprague.com>
2022-11-19 12:22:54 -0500

    repair account auth

--- index.mjs
+++ index.mjs
@@ -17,7 +17,7 @@ export default class DataServer extends Componentry.Module {
         let router = express.Router();
         router.get('/data/:collection/:item?',async(req,res)=>{
             try {
-                let result = await this.get(req.account.id,req.params.collection,req.params.item,req.query);
+                let result = await this.get(req.account,req.params.collection,req.params.item,req.query);
                 res.json(result);
             } catch(e) {
                 res.status(e.status||500).json({status:"error",message:e.message});
@@ -25,7 +25,7 @@ export default class DataServer extends Componentry.Module {
         });
         router.put('/data/:collection/:item?',async(req,res)=>{
             try {
-                let result = await this.put(req.account.id,req.params.collection,req.body,req.params.item);
+                let result = await this.put(req.account,req.params.collection,req.body,req.params.item);
                 res.json(result);
             } catch(e) {
                 res.status(e.status||500).json({status:"error",message:e.message});
@@ -33,7 +33,7 @@ export default class DataServer extends Componentry.Module {
         });
         router.delete('/data/:collection/:item',async(req,res)=>{
             try {
-                await this.remove(req.account.id,req.params.collection,req.params.item);
+                await this.remove(req.account,req.params.collection,req.params.item);
                 res.status(204).send();
             } catch(e) {
                 res.status(e.status||500).json({status:"error",message:e.message});
@@ -55,7 +55,9 @@ export default class DataServer extends Componentry.Module {
      * @returns Object
      */
     async get(account,collection,item,options={}) {
-        let selector = {_account:account.id};
+        let accounts = await this.connector.acl.get.read({user:account.userId},"account");
+        accounts = accounts.map(a=>a._id.account);
+        let selector = {_account:{$in:accounts}};
         if (item) selector._id = item;
         if (options.where) Object.assign(selector,this.parser.objectify(options.where));
         let sort = this.parser.sortify(options.sort);
@@ -90,6 +92,10 @@ export default class DataServer extends Componentry.Module {
      * The request is constructed as an upsert. If no item id is provided,
      * one is generated using Identifier.new.
      *
+     * All objects should have and _account attribute, or one is assigned based
+     * on the current account. PUT ensures the current user as rights to select
+     * or modify this account.
+     *
      * @param account context.
      * @param collection the name of the collection to collect data from.
      * @param body an object or array of objects
@@ -97,11 +103,14 @@ export default class DataServer extends Componentry.Module {
      * @returns Object
      */
     async put(account,collection,body,id) {
+        let accounts = await this.connector.acl.get.read({user:account.userId},"account");
+        accounts = accounts.map(a=>a._id.account);
         if (Array.isArray(body)) {
             if (body.length === 0) throw new Error("Empty data set");
             let writes = [];
             for (let o of body) {
                 if (!o._account) o._account = account.id;
+                else if (!accounts.includes(o._account)) continue;
                 writes.push({updateOne:{
                     filter:{_id:(o._id||this.connector.idForge.datedId())},
                     update:constructModifier(o),
@@ -113,8 +122,13 @@ export default class DataServer extends Componentry.Module {
         } else {
             let selector = {_id:body._id||id||this.connector.idForge.datedId()};
             if (!body._account) body._account = account.id;
+            else if (!accounts.includes(body._account)) {
+                let error = new Error();
+                error.status = 401;
+                throw error;
+            }
             let modifier = constructModifier(body);
-            let options = {returnOriginal:false,returnDocument:"after",upsert:true};
+            let options = {upsert:true,returnNewDocument:true};
             let result = await this.connector.db.collection(collection).findOneAndUpdate(selector,modifier,options);
             return result.value;
         }
@@ -123,10 +137,10 @@ export default class DataServer extends Componentry.Module {
             let modifier = {$set:{}};
             for (let a in doc) {
                 if (['$push','$pull','$addToSet','$unset','$set'].includes(a)) modifier[a] = doc[a];
-                else if (!['_id','_created','_createdBy','_account'].includes(a)) modifier.$set[a] = doc[a];
+                else if (!['_id','_created','_createdBy'].includes(a)) modifier.$set[a] = doc[a];
             }
             modifier.$set._modified = new Date();
-            modifier.$setOnInsert = {_created:new Date(),_account:doc._account};
+            modifier.$setOnInsert = {_created:new Date()};
             if (!doc._createdBy) modifier.$setOnInsert._createdBy = account.userId;
             return modifier;
         }
